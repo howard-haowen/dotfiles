6bef089ab8b6f03d5e0a587fb138e33db3997236
Haowen Jiang <howard.haowen@gmail.com>
2024-10-03 13:01:09 +0800

    Remove the neovide plugin for neovim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -12,5 +12,4 @@ return {
   { import = "astrocommunity.note-taking.obsidian-nvim" },
   { import = "astrocommunity.editing-support.comment-box-nvim" },
   { import = "astrocommunity.editing-support.true-zen-nvim" },
-  { import = "astrocommunity.recipes.neovide" },
 }
