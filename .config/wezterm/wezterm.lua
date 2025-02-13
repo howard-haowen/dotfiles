0583081d5fdd79a166fdd6b4b32aaffc0826fa2c
Haowen Jiang <howard.haowen@gmail.com>
2025-02-13 14:37:14 +0800

    Add the bar plugin for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -26,6 +26,7 @@ config.window_padding = {
 	top = 10,
 	bottom = 10,
 }
+config.window_close_confirmation = "NeverPrompt"
 
 -- Background Image Configuration
 local dimmer = { brightness = 0.2 }
@@ -45,20 +46,87 @@ config.background = {
 }
 
 -- Plugins
--- Rather simple presentation mode toggle for WezTerm.
+-- Presentation: Rather simple presentation mode toggle for WezTerm.
 -- Usage: Activate with CTRL + ALT + P for presentation mode, or CTRL + ALT + SHIFT + P for fullscreen presentation.
 wezterm.plugin.require("https://gitlab.com/xarvex/presentation.wez").apply_to_config(config)
 
--- statusbar.apply_to_config(config, {
--- 	cells = {
--- 		weather = {
--- 			enabled = true,
--- 			format = "Weather: %s",
--- 			update_interval = 600, -- Update every 10 minutes
--- 			command = 'curl -s "wttr.in/Taipei?format=3"',
--- 		},
--- 	},
--- })
---
+-- Modal: Vim-like modal keybindings for your terminal! ✌️
+-- wezterm.plugin.require("https://github.com/MLFlexer/modal.wezterm").apply_to_config(config)
+
+-- Bar: A wezterm tab bar with batteries included
+-- Bar's default config
+local bar_config = {
+	position = "bottom",
+	max_width = 32,
+	padding = {
+		left = 1,
+		right = 1,
+	},
+	separator = {
+		space = 1,
+		left_icon = wezterm.nerdfonts.fa_long_arrow_right,
+		right_icon = wezterm.nerdfonts.fa_long_arrow_left,
+		field_icon = wezterm.nerdfonts.indent_line,
+	},
+	modules = {
+		tabs = {
+			active_tab_fg = 4,
+			inactive_tab_fg = 6,
+		},
+		workspace = {
+			enabled = true,
+			icon = wezterm.nerdfonts.cod_window,
+			color = 8,
+		},
+		leader = {
+			enabled = true,
+			icon = wezterm.nerdfonts.oct_rocket,
+			color = 2,
+		},
+		pane = {
+			enabled = true,
+			icon = wezterm.nerdfonts.cod_multiple_windows,
+			color = 7,
+		},
+		username = {
+			enabled = true,
+			icon = wezterm.nerdfonts.fa_user,
+			color = 6,
+		},
+		hostname = {
+			enabled = false,
+			icon = wezterm.nerdfonts.cod_server,
+			color = 8,
+		},
+		clock = {
+			enabled = true,
+			icon = wezterm.nerdfonts.md_calendar_clock,
+			color = 5,
+		},
+		cwd = {
+			enabled = true,
+			icon = wezterm.nerdfonts.oct_file_directory,
+			color = 7,
+		},
+		spotify = {
+			enabled = false,
+			icon = wezterm.nerdfonts.fa_spotify,
+			color = 3,
+			max_width = 64,
+			throttle = 15,
+		},
+	},
+}
+wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm").apply_to_config(config, bar_config)
+
+-- Key Bindings for vim-like navigation
+-- config.leader = { key = "a", mods = "ALT", timeout_milliseconds = 1000 } -- Set leader key to Ctrl
+-- config.keys = {
+-- 	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+-- 	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+-- 	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+-- 	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+-- }
+
 -- Return the config object
 return config
