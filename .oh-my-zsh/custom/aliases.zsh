cd065c99bf04ec4f73724dcdcb98b4ac2dbffc53
Haowen Jiang <howard.haowen@gmail.com>
2025-11-27 17:47:16 +0800

    Add the ggraph alias for serie, a git graph on the terminal

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -30,5 +30,6 @@ alias orc='orchestrate'
 alias nom='nom -c ~/dotfiles/.config/nom/config.'
 alias goread='goread -c ~/dotfiles/.config/goread/colorscheme.json -s ~/dotfiles/.config/goread/goread.yml -r ~/dotfiles/.config/goread/urls.yml'
 alias da='wtfutil'
+alias ggraph='serie'
 # Use fzf with the preview_file function, defined in custom/preview.zsh
 # alias ff="fzf --border --preview 'preview_file {}'"
